5833a9bceb660838c8477b7c9e61c713f2b3542e
nfourteen <dave@nfourteen.com>
2018-06-05 21:52:52 -0400

    Player and Enemy objects added with inital state

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, sprite) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.offsetY = 30;
+    this.x = x ? x : 0;
+    this.y = y ? y : 50;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = sprite ? sprite : 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +27,53 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 202;
+    this.y = 385;
+    this.offsetY = 30;
+    this.sprite = 'images/char-boy.png';
+    this.bounds = {xMin: 0, xMax: 404, yMin: 45, yMax: 385};
+};
 
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(movement) {
+    console.log(movement);
+    switch (movement) {
+        case 'up':
+            this.y -= 85;
+            break;
+        case 'down':
+            this.y += 85;
+            break;
+        case 'left':
+            this.x -= 101;
+            break;
+        case 'right':
+            this.x += 101;
+            break;
+    }
+    console.log(this);
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
+const enemy1 = new Enemy(0, 60);
+const enemy2 = new Enemy(0, 145);
+const enemy3 = new Enemy(0, 227);
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
 
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
